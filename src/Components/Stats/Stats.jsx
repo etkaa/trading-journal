07fd2140d45dbff0d0238ea35ad90baf6a4f1147
manfromny <ebacikoz@gmail.com>
2022-09-02 23:52:46 -0400

    added dynamic current P/L statistic

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, Fragment } from "react";
 import { UserContext } from "../../Context/User.Context";
 import { StatsContainer } from "./Stats.styles";
 import StatCard from "../StatCard/StatCard";
@@ -6,21 +6,16 @@ import { getUserStats } from "../../Utils/ApiRequests";
 import Spinner from "../Spinner/Spinner";
 
 const defaultUserStats = {
-  currentPandL: "",
-  currentBalance: "",
-  winLossRatio: "",
-  averageRiskReward: "",
+  currentPandL: "0",
+  currentBalance: "0",
+  winLossRatio: "0",
+  averageRiskReward: "Calculating",
 };
 
 const Stats = () => {
   const { currentUser, toggler } = useContext(UserContext);
   const [userStats, setUserStats] = useState(defaultUserStats);
   const [loading, setLoading] = useState(false);
-
-  const initialBalance = currentUser.profile.initialBalance;
-  const currentBalance =
-    Number(initialBalance) + Number(userStats.sumOfAllTrades);
-  const winLossRatio = Number(userStats.winLossRatio);
 
   useEffect(() => {
     setLoading(true);
@@ -32,35 +27,44 @@ const Stats = () => {
     fetchData();
   }, [currentUser, setUserStats, toggler]);
 
+  const initialBalance = currentUser.profile.initialBalance;
+  const currentBalance =
+    Number(initialBalance) + Number(userStats.sumOfAllTrades);
+  const winLossRatio = Number(userStats.winLossRatio);
+  const currentPandL =
+    (Number(userStats.sumOfAllTrades) / Number(initialBalance)) * 100;
+  const averageRiskReward = userStats.averageRiskReward;
+
   console.log(userStats);
 
   return (
     <StatsContainer>
-      <StatCard
-        statTitle={"Current P/L"}
-        stat={"+ %6"}
-        statDetail={"August 2022"}
-      />
-      {!loading ? (
-        <StatCard
-          statTitle={"Current Balance"}
-          stat={`$${currentBalance.toLocaleString()}`}
-          statDetail={currentUser.profile.brokerName}
-        />
-      ) : (
+      {loading ? (
         <Spinner />
+      ) : (
+        <Fragment>
+          <StatCard
+            statTitle={"Current P/L"}
+            stat={`%${currentPandL}`}
+            statDetail={"August 2022"}
+          />
+          <StatCard
+            statTitle={"Current Balance"}
+            stat={`$${currentBalance.toLocaleString()}`}
+            statDetail={currentUser.profile.brokerName}
+          />
+          <StatCard
+            statTitle={"Win/Loss Ratio"}
+            stat={`%${winLossRatio.toFixed(2)}`}
+            statDetail={"YTD 2022"}
+          />
+          <StatCard
+            statTitle={"Average Risk/Reward"}
+            stat={averageRiskReward}
+            statDetail={"All Times"}
+          />
+        </Fragment>
       )}
-
-      <StatCard
-        statTitle={"Win/Loss Ratio"}
-        stat={`%${winLossRatio.toFixed(2)}`}
-        statDetail={"YTD 2022"}
-      />
-      <StatCard
-        statTitle={"Average Risk/Reward"}
-        stat={"1 / 3.76"}
-        statDetail={"All Times"}
-      />
     </StatsContainer>
   );
 };
